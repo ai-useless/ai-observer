78fa7cba8e6c187b7594cc261bbe70ac2331765f
Zhao KK <kikakkz@hotmail.com>
2025-04-16 23:17:35 +0800

    Add basic setting to control box display

--- webui/src/components/footer/Footer.vue
+++ webui/src/components/footer/Footer.vue
@@ -2,7 +2,7 @@
   <div class='row items-center' style='padding-bottom: 16px;'>
     <RoundTable />
     <q-space />
-    <TopicInput />
+    <TopicInput v-if='!inScratch' />
     <q-space />
     <q-avatar class='cursor-pointer' size='24px'>
       <q-img :src='githubLogo' />
@@ -11,11 +11,15 @@
 </template>
 
 <script setup lang='ts'>
+import { computed } from 'vue'
+import { setting } from 'src/localstore'
 
 import RoundTable from '../seminar/RoundTable.vue'
 import TopicInput from '../seminar/TopicInput.vue'
 
 import { githubLogo } from 'src/assets'
+
+const inScratch = computed(() => setting.Setting.inScratch())
 
 </script>
 
