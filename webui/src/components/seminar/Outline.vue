ddc569f42bd2f94a99407691f42697c9ae7b5a83
Zhao KK <kikakkz@hotmail.com>
2025-04-19 19:01:12 +0800

    Correct topic

--- webui/src/components/seminar/Outline.vue
+++ webui/src/components/seminar/Outline.vue
@@ -1,7 +1,7 @@
 <template>
   <div style='max-width: 280px'>
     <div style='margin-top: 16px;'>
-      <div v-for='(title, index) in titles' :key='title' :class='["cursor-pointer title", index === activeIndex ? "text-grey-9 text-bold" : "text-grey-7 normal"]' style='margin-left: 24px; margin-top: 8px;'>
+      <div v-for='title in titles' :key='title' :class='["cursor-pointer title", title === activeTopic ? "text-grey-9 text-bold" : "text-grey-7 normal"]' style='margin-left: 24px; margin-top: 8px;'>
         <div v-html='title' style='font-size: 12px;' />
       </div>
     </div>
@@ -13,12 +13,12 @@ import { computed, toRef } from 'vue'
 
 interface Props {
   json: Record<string, unknown>
-  activeIndex: number
+  activeTopic: string
 }
 // eslint-disable-next-line no-undef
 const props = defineProps<Props>()
 const json = toRef(props, 'json')
-const activeIndex = toRef(props, 'activeIndex')
+const activeTopic = toRef(props, 'activeTopic')
 const titles = computed(() => json.value.titles as string[])
 
 </script>
