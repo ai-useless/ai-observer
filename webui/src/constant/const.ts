107697ac88e35c8418436505971426823b969b81
Zhao KK <kikakkz@hotmail.com>
2025-04-20 19:10:30 +0800

    Update tts api

--- webui/src/constant/const.ts
+++ webui/src/constant/const.ts
@@ -1,2 +1,2 @@
 export const FALLBACK_API = 'http://210.209.69.40:8091/api/get-response'
-export const AUDIO_API = 'http://210.209.69.40:8000/generate'
+export const TEXT2SPEECH_API = 'http://210.209.69.40:8000/speak'
