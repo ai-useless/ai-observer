4221ed8bd5de02e65e3d62dc476a66ce236f36df
Zhao KK <kikakkz@hotmail.com>
2025-04-20 19:45:25 +0800

    Add publish command

--- tts/build.sh
+++ tts/build.sh
@@ -2,3 +2,6 @@
 
 # chutes build observer_tts:chute --local
 # docker run --gpus all --rm -it -e CHUTES_EXECUTION_CONTEXT=REMOTE -p 8000:8000 observer-tts:0.0.2 chutes run observer_tts:chute --port 8000 --dev
+
+# chutes build observer_tts:chute --public --wait --debug
+# chutes deploy observer_tts:chute --public
