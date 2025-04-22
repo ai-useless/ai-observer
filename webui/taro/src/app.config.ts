1d64f940b307258abe065b490fe058bc0ae4da1b
Zhao KK <kikakkz@hotmail.com>
2025-04-22 14:04:39 +0800

    Add taro application

---
+++ webui/taro/src/app.config.ts
@@ -0,0 +1,11 @@
+export default {
+  pages: [
+    'pages/index/IndexPage'
+  ],
+  window: {
+    backgroundTextStyle: 'light',
+    navigationBarBackgroundColor: '#fff',
+    navigationBarTitleText: 'WeChat',
+    navigationBarTextStyle: 'black'
+  }
+}
