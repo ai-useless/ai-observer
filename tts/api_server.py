430f3269ea9bbf8b647a10d38c33332a78c40c93
jakys <jakysing@163.com>
2025-04-22 12:02:04 +0800

    Fix tts

--- tts/api_server.py
+++ tts/api_server.py
@@ -11,27 +11,27 @@ import io
 API_URL="http://127.0.0.1:8800/speak"
 CHUTES_API_TOKEN="cpk_69adda47d49b4d51b3c6ae01f5449cdd.b167f56b3e8d5ffa88bf5cc6513bb6f4.1VTnR4OGz56pv1GlHG5OVDMdTrvSpLI5"
 
-# 初始化FastAPI
+# Init FastAPI
 app = FastAPI(title="CoquiTTS-API", version="1.0.0")
 
-# 开发环境允许所有源，生产环境需指定域名
-allowed_origins = [
-    "http://localhost:*",
-    "https://your-domain.com",
-    "https://*.your-domain.com"
-]
+# Production env
+# allowed_origins = [
+#     "http://localhost:*",
+#     "https://your-domain.com",
+#     "https://*.your-domain.com"
+# ]
 
-# 生产环境
+# Production env
 # app.add_middleware(
 #     CORSMiddleware,
-#     allow_origins=allowed_origins,  # 网页8建议生产环境限制域名
-#     allow_credentials=True,          # 支持cookie/token认证场景
-#     allow_methods=["*"],            # 允许所有HTTP方法
-#     allow_headers=["*"],            # 允许所有请求头
-#     expose_headers=["Content-Disposition"]  # 网页7建议暴露特殊头
+#     allow_origins=allowed_origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+#     expose_headers=["Content-Disposition"]
 # )
 
-# 开发环境，允许所有来源和请求
+# Dev env
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -53,23 +53,20 @@ def get_audio_data(text, voice):
       "text": text,
       "voice": voice
     }
-    print(f"body: {body}")
     response = requests.post(API_URL, headers=headers, json=body, stream=True)
     if response.status_code == 200:
-        return response.content  # 直接获取二进制内容
+        return response.content
     else:
-        raise Exception(f"API请求失败，状态码：{response.status_code}")
-    
+        raise Exception(f"Request failed：{response.status_code}")
+
 @app.post("/speak")
 async def generate_audio(params: TTSParams):
     try:
-        print("params: ", params)
         audio_bytes = get_audio_data(params.text, params.speakerVoice)
         buffer = io.BytesIO(audio_bytes)
         sample_rate, audio_data = wavfile.read(buffer)
-        duration = round(len(audio_data) / sample_rate, 3)  # 精确到毫秒
+        duration = round(len(audio_data) / sample_rate, 3)
         
-        # 3. Base64编码
         base64_str = base64.b64encode(audio_bytes).decode('utf-8')
         return {
             "type": "audio/wav",
