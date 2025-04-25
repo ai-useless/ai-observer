b400c967a896ddd0bc2c98d9bdb848f207f31297
jakys <jakysing@163.com>
2025-04-25 16:34:50 +0800

    Fix api

--- webui/quasar/src/constant/const.ts
+++ webui/quasar/src/constant/const.ts
@@ -1,2 +1,2 @@
-export const FALLBACK_API = 'http://210.209.69.40:8091/api/get-response'
-export const TEXT2SPEECH_API = 'http://210.209.69.40:8000/speak'
+export const FALLBACK_API = 'http://210.209.69.40:8091/api/v1/chat'
+export const TEXT2SPEECH_API = 'http://210.209.69.40:8900/speak'
