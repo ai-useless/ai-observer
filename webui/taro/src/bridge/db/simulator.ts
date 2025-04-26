054e702dee00959e22a48379ff23ed54bb3b97ca
Zhao KK <kikakkz@hotmail.com>
2025-04-26 20:37:56 +0800

    Add voice

--- webui/taro/src/bridge/db/simulator.ts
+++ webui/taro/src/bridge/db/simulator.ts
@@ -8,47 +8,47 @@ export class _Simulator {
         'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBWoHax_26D4cf1C5S_7bkARewi9-SijOSlw&s',
       personality: '客观理性的AGI智障频道AGI观点栏目主持人',
       host: true,
-      speakerVoice: 'zm_031',
+      speakerVoice: 'huyihu',
       archetype: '胡一虎',
       title: '凤凰卫视一虎一席谈节目主持人'
     },
     {
-      name: '猪头三',
+      name: '周杰棍',
       avatar:
         'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBWoHax_26D4cf1C5S_7bkARewi9-SijOSlw&s',
       personality: '知识丰富，关心前沿科技的科技公司创始人',
       host: false,
-      speakerVoice: 'zm_010',
+      speakerVoice: 'jay',
       archetype: '朱重八',
       title: '明朝开国皇帝朱元璋'
     },
     {
-      name: '安若素',
+      name: '李云蛇',
       avatar:
         'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBWoHax_26D4cf1C5S_7bkARewi9-SijOSlw&s',
       personality: '国防大学教授，国务院特殊津贴专家',
       host: false,
-      speakerVoice: 'zm_012',
+      speakerVoice: 'liyunlong',
       archetype: '安又琪',
       title: '超级女声选秀比赛冠军'
     },
     {
-      name: '九头鸟',
+      name: '米奇',
       avatar:
         'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBWoHax_26D4cf1C5S_7bkARewi9-SijOSlw&s',
       personality: '科技爱好者，民间科学家',
       host: false,
-      speakerVoice: 'zf_027',
+      speakerVoice: 'miqi',
       archetype: '九头鸟',
       title: '认为自己可以推翻相对论的民间科学家'
     },
     {
-      name: '陆一讯',
+      name: '非小弟',
       avatar:
         'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBWoHax_26D4cf1C5S_7bkARewi9-SijOSlw&s',
       personality: '时事新闻媒体特约撰稿人',
       host: false,
-      speakerVoice: 'zf_044',
+      speakerVoice: 'xiaoge',
       archetype: '鲁迅',
       title: '愤世嫉俗的文艺青年'
     }
