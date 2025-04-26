da0919ef83eba80bffcc9788466e208655fb44d4
Zhao KK <kikakkz@hotmail.com>
2025-04-26 22:23:26 +0800

    Correct audio url

--- webui/taro/src/constant/const.ts
+++ webui/taro/src/constant/const.ts
@@ -1,3 +1,2 @@
-// export const FALLBACK_API = 'http://47.117.36.214:8091/api/v1/chat'
-export const FALLBACK_API = 'http://172.16.31.73:8091/api/v1/chat'
-export const TEXT2SPEECH_API = 'http://172.16.31.73:8091/api/v1/speak'
+export const FALLBACK_API = 'http://47.117.36.214:8091/api/v1/chat'
+export const TEXT2SPEECH_API = 'http://47.117.36.214:8091/api/v1/speak'
