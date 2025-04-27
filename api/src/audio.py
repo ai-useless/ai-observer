6e6eda84cd69eac25f7bd04bdda11d202651dc25
jakys <jakysing@163.com>
2025-04-27 14:32:12 +0800

    Fix request api

--- api/src/audio.py
+++ api/src/audio.py
@@ -5,6 +5,7 @@ import asyncio
 import hashlib
 from pydub import AudioSegment
 import io
+import random
 
 def purify_text(text):
     soup = BeautifulSoup(text, 'html.parser')
@@ -41,7 +42,15 @@ def chunk_text(text, chunk_size=100):
 
     return chunks
 
-async def fetch_audio(text: str, voice: str, api_token: str, session: aiohttp.ClientSession, semaphore: asyncio.Semaphore) -> bytes:
+async def fetch_audio(
+        text: str, 
+        voice: str, 
+        api_token: str, 
+        session: aiohttp.ClientSession, 
+        semaphore: asyncio.Semaphore,
+        min_delay_ms: float = 50,
+        max_delay_ms: float = 300
+        ) -> bytes:
     url = 'https://kikakkz-cosy-voice-tts.chutes.ai/speak'
     headers = {
         'Authorization': f'Bearer {api_token}',
@@ -51,14 +60,15 @@ async def fetch_audio(text: str, voice: str, api_token: str, session: aiohttp.Cl
         'text': text,
         'voice': voice
     }
-    print(f"--payload: {payload} post {url}")
     timeout = aiohttp.ClientTimeout(connect=10, total=59)
     async with semaphore:
         try:
+            delay_seconds = random.uniform(min_delay_ms, max_delay_ms) / 1000
+            await asyncio.sleep(delay_seconds)
+
             async with session.post(url, json=payload, timeout=timeout, headers=headers) as response:
                 response.raise_for_status()
                 audio_bytes = await response.read()
-                print(f"len(audio_bytes): {len(audio_bytes)}")
                 return audio_bytes
         except Exception as e:
             print(f"Request exception：{url}，error：{str(e)}")
@@ -81,7 +91,6 @@ async def concurrent_audio_requests(chunks: list[str], voice: str, api_token: st
         return sorted_results
 
 def merge_audio_buffers(audio_buffers: list[bytes], output_path: str) -> str:
-    print(f"--create fid")
     valid_buffers = [b for b in audio_buffers if b]
     hasher = hashlib.sha256()
     for buffer in valid_buffers:
@@ -90,7 +99,6 @@ def merge_audio_buffers(audio_buffers: list[bytes], output_path: str) -> str:
     file_name = hasher.hexdigest() + ".wav"
     output_path = f"{output_path}/{file_name}"
 
-    print(f"--merge audio: {output_path}")
     combined = None
     for i, buffer in enumerate(audio_buffers):
         if not buffer:
