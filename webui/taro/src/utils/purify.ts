16cf9284fa16896779104c1c5be9ac401ffe0f1d
hong-t <th13564039482@163.com>
2025-04-27 16:25:34 +0800

    Correct typing interval

--- webui/taro/src/utils/purify.ts
+++ webui/taro/src/utils/purify.ts
@@ -3,5 +3,7 @@ import * as cheerio from 'cheerio'
 export const purifyText = (html: string): string => {
   const $ = cheerio.load(html)
   $('script').remove()
+  $('sup').remove()
+  $('think').remove()
   return $.text().trim()
 }
