2c3d59dd7dd25e3bbcc35b32346a45390ace1336
Zhao KK <kikakkz@hotmail.com>
2025-04-28 10:49:16 +0800

    Add seminar page

--- webui/taro/src/pages/index/IndexPage.vue
+++ webui/taro/src/pages/index/IndexPage.vue
@@ -1,28 +1,23 @@
 <template>
-  <View v-if='tabIndex >= 0 && showMe'>
-    <Seminar />
+  <View>
+    HHHHHHHHHHHH
   </View>
 </template>
 
 <script setup lang='ts'>
 import { View } from '@tarojs/components'
-import { computed, ref } from 'vue'
+import { computed } from 'vue'
 import { setting } from 'src/localstores'
 import Taro, { useDidShow } from '@tarojs/taro'
 
 import './index.sass'
-import Seminar from '../../components/seminar/Seminar.vue'
 
 const tabIndex = computed(() => setting.Setting.tabIndex())
-const showMe = ref(false)
 
 useDidShow(() => {
   if (tabIndex.value < 0) {
-    showMe.value = false
     setting.Setting.setTabIndex(1)
     Taro.switchTab({ url: '/pages/seminar/SeminarPage' })
-  } else {
-    showMe.value = true
   }
 })
 
