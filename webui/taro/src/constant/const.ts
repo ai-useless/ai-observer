56e7fd4d2c8f37fef4b3f993b2d023f8bd930797
Zhao KK <kikakkz@hotmail.com>
2025-04-28 19:52:48 +0800

    Support cook audio

--- webui/taro/src/constant/const.ts
+++ webui/taro/src/constant/const.ts
@@ -1,2 +1,3 @@
 export const FALLBACK_API = 'http://47.117.36.214:8091/api/v1/chat'
 export const TEXT2SPEECH_API = 'http://47.117.36.214:8091/api/v1/speak'
+export const COOK_AUDIO_API = 'http://47.117.36.214:8091/api/v1/cook_audio'
