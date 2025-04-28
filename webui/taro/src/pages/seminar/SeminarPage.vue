cc5aae58e0b2fed0cb23c9e283301b749d193491
Zhao KK <kikakkz@hotmail.com>
2025-04-28 10:49:16 +0800

    Add seminar page

--- webui/taro/src/pages/seminar/SeminarPage.vue
+++ webui/taro/src/pages/seminar/SeminarPage.vue
@@ -1,7 +1,13 @@
 <template>
-  <View>HHHHH</View>
+  <View>
+    <Seminar />
+  </View>
 </template>
 
 <script setup lang='ts'>
 import { View } from '@tarojs/components'
+
+import './seminar.sass'
+import Seminar from '../../components/seminar/Seminar.vue'
+
 </script>
