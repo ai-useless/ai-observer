acfb97060f04e597b309d809a4e3e0e3cbeab316
Zhao KK <kikakkz@hotmail.com>
2025-04-28 01:01:45 +0000

    Add include py for common code

---
+++ api/src/include.py
@@ -0,0 +1,17 @@
+import sys
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+common_dir = os.path.abspath(
+    os.path.join(
+        os.path.join(
+            os.path.join(current_dir, '..'), '..',
+        ), 'common',
+    ),
+)
+if common_dir not in sys.path:
+    sys.path.append(common_dir)
+
+from purify_text import purify_text, chunk_text
+
+__all__ = ['purify_text', 'chunk_text']
