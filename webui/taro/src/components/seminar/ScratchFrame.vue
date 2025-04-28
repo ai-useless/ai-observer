0b17c9e490077ec8298df6a63c3d75bd821cc9ec
Zhao KK <kikakkz@hotmail.com>
2025-04-28 13:30:34 +0800

    Support generate topics

--- webui/taro/src/components/seminar/ScratchFrame.vue
+++ webui/taro/src/components/seminar/ScratchFrame.vue
@@ -26,15 +26,31 @@
         v-for='clazz in presetClasses'
         :key='clazz'
         @click='onGenerateTopics(clazz)'
+        :disabled='generating'
+        :style='{ color: generating ? "gray" : "black" }'
       >
         {{ clazz }}
       </Button>
+    </View>
+    <View style='margin-top: 16px;' v-if='topicType.length'>
+      <View style='width: 100%; display: flex; justify-content: space-between; align-items: center; border-bottom: 1px solid gray; padding-bottom: 4px;'>
+        <View class='title'>{{ topicType }}</View>
+        <View class='title' style='color: blue' @click='onChangeTopicsClick'>{{ generating ? '生成中...' : '换一批' }}</View>
+      </View>
+      <View v-if='topics.length' style='margin-top: 8px;'>
+        <View v-for='_topic in topics' style='font-size: 14px; color: blue;'>
+          {{ _topic }}
+        </View>
+      </View>
+      <View v-else class='title' style='height: 200px; display: flex; justify-content: center; align-items: center;'>
+        AGI正在为您生成话题...
+      </View>
     </View>
   </View>
 </template>
 
 <script setup lang='ts'>
-import { dbBridge } from 'src/bridge'
+import { dbBridge, entityBridge } from 'src/bridge'
 import { seminar, setting } from 'src/localstores'
 import { ref, watch, computed } from 'vue'
 import { View, Input, Button, Text } from '@tarojs/components'
@@ -70,7 +86,7 @@ const initialTopics = [
 ]
 
 const presetClasses = [
-  '随便听点儿什么',
+  '随便讨论点儿什么',
   '历史',
   '二战',
   '量子物理',
@@ -78,12 +94,18 @@ const presetClasses = [
   '关于夏朝',
   '大航海时代',
   '文艺复兴',
-  '羊吃人运动'
+  '羊吃人运动',
+  '微博热点',
+  '央视热点',
+  '今日热搜'
 ]
 
 const _seminar = computed(() => dbBridge._Seminar.seminar(seminar.Seminar.seminar()))
 const topic = ref(_seminar.value?.topic || initialTopics[Math.floor(Math.random() * initialTopics.length)])
 const tabIndex = computed(() => setting.Setting.tabIndex())
+const topicType = ref('')
+const historyTopics = ref([] as string[])
+const topics = ref([] as string[])
 
 const handleInput = (e: { detail: { value: string } }) => {
   topic.value = e.detail.value
@@ -111,8 +133,29 @@ useDidShow(async () => {
   }
 })
 
-const onGenerateTopics = (clazz: string) => {
-  console.log(clazz)
+const generating = ref(false)
+
+const generateTopics = async () => {
+  let clazz = topicType.value
+  if (topicType.value === presetClasses.values[0]) clazz = '随机不同领域可以输出观点的话题'
+  generating.value = true
+  topics.value = await entityBridge.Topic.generateTopics(clazz, 10, historyTopics.value)
+  historyTopics.value.push(...topics.value)
+  generating.value = false
+}
+
+const onGenerateTopics = async (clazz: string) => {
+  if (generating.value) return
+
+  historyTopics.value = []
+  topicType.value = clazz
+
+  await generateTopics()
+}
+
+const onChangeTopicsClick = async () => {
+  if (generating.value) return
+  await generateTopics()
 }
 
 </script>
