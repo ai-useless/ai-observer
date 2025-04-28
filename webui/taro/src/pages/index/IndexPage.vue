2d1fed67405637d1af6faf0f76fad83f21753365
Zhao KK <kikakkz@hotmail.com>
2025-04-28 10:58:58 +0800

    Add index page

--- webui/taro/src/pages/index/IndexPage.vue
+++ webui/taro/src/pages/index/IndexPage.vue
@@ -1,23 +1,27 @@
 <template>
-  <View>
-    HHHHHHHHHHHH
+  <View v-if='tabIndex >= 0 && showMe'>
+    <Index />
   </View>
 </template>
 
 <script setup lang='ts'>
 import { View } from '@tarojs/components'
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 import { setting } from 'src/localstores'
 import Taro, { useDidShow } from '@tarojs/taro'
 
 import './index.sass'
+import Index from 'src/components/index/Index.vue'
 
 const tabIndex = computed(() => setting.Setting.tabIndex())
+const showMe = ref(false)
 
-useDidShow(() => {
+useDidShow(async () => {
   if (tabIndex.value < 0) {
     setting.Setting.setTabIndex(1)
     Taro.switchTab({ url: '/pages/seminar/SeminarPage' })
+  } else {
+    showMe.value = true
   }
 })
 
