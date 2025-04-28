5abc4a8d308d962630a62214ab9ba999c3bec4d8
Zhao KK <kikakkz@hotmail.com>
2025-04-28 13:30:15 +0000

    Allow creator cook simulator

--- api/src/cook_simulator.py
+++ api/src/cook_simulator.py
@@ -23,7 +23,7 @@ async def audio_2_text(audio_b64: str) -> str:
             response.raise_for_status()
             return (await response.json())['text']
 
-async def cook_simulator(code: str, username: str, avatar: str, audio_b64: str, simulator: str, simulator_avatar: str):
+async def cook_simulator(code: str, username: str, avatar: str, audio_b64: str, simulator: str, simulator_avatar: str, personality: str | None = None):
     url = f'https://api.weixin.qq.com/sns/jscode2session?appid={config.weapp_id}&secret={config.weapp_secret}&js_code={code}&grant_type=authorization_code'
 
     timeout = aiohttp.ClientTimeout(connect=10, total=30)
@@ -60,7 +60,7 @@ async def cook_simulator(code: str, username: str, avatar: str, audio_b64: str, 
                 f.write(simulator_avatar_cid)
 
             # TODO: get origin personality
-            personality = '普普通通路人甲'
+            personality = '普普通通路人甲' if personality is None else personality
             db.new_audio(openid, username, avatar, file_cid, text, simulator, simulator_avatar_cid, personality)
 
             # TODO: automatically review audio by another AI
