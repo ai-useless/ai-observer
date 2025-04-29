6598fe98a01c7e72e04196ff4ca473099fc6d041
Zhao KK <kikakkz@hotmail.com>
2025-04-29 17:06:07 +0800

    Add action

--- webui/taro/src/components/simulator/SimulatorCard.vue
+++ webui/taro/src/components/simulator/SimulatorCard.vue
@@ -8,7 +8,7 @@
     <View style='display: flex; align-items: center; margin-top: 4px;'>
       <Image :src='_simulator.wechat_avatar' style='width: 24px; height: 24px; border-radius: 50%;' />
       <Text style='font-size: 12px; color: gray; font-weight: 600; margin-left: 4px;'>{{ _simulator.wechat_username }}</Text>
-      <Text style='font-size: 12px; color: gray; margin-left: 4px;'>{{ timestamp.timestamp2HumanReadable(_simulator.timestamp) }}</Text>
+      <Text style='font-size: 12px; color: gray; margin-left: 4px;'>{{ timestamp.timestamp2HumanReadable(_simulator.timestamp) }}创建</Text>
     </View>
   </View>
 </template>
