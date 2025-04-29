09b74a0a180d4f4ea8b5acf68016dabec8760b39
jakys <jakysing@163.com>
2025-04-29 15:32:04 +0800

    mv stt

--- tts/build_whisper.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Whisper
-
-# chutes build whisper_stt:chute --local
-# docker run --gpus all --rm -it -e CHUTES_EXECUTION_CONTEXT=REMOTE -p 8900:8000 whisper-stt:0.0.1 chutes run whisper_stt:chute --port 8000 --dev
-
-# chutes build whisper_stt:chute --public --wait --debug
-# chutes deploy whisper_stt:chute --public
