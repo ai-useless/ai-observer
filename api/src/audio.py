4980cc4a0e4fd6e22bb7b6ddd9a097a1f5774638
Zhao KK <kikakkz@hotmail.com>
2025-04-30 20:59:44 +0800

    Correct speak api

--- api/src/audio.py
+++ api/src/audio.py
@@ -32,7 +32,7 @@ class AudioGenerate:
             min_delay_ms: float = 50,
             max_delay_ms: float = 300
             ) -> bytes:
-        url = 'https://kikakkz-cosy-voice-tts.chutes.ai/speak'
+        url = 'https://kikakkz-cosy-voice-tts.chutes.ai/v1/speak'
         headers = {
             'Authorization': f'Bearer {config.api_token}',
             'Content-Type': 'application/json'
