2b8606189db5979e693da0266e66aafed155802d
Zhao KK <kikakkz@hotmail.com>
2025-04-30 18:57:05 +0800

    Correct voice argument

--- api/src/audio.py
+++ api/src/audio.py
@@ -18,6 +18,7 @@ class AudioGenerate:
 
         file_name = self.merge_audio_buffers(
             audio_buffers=[b for b in audio_buffers if b],
+            voice=voice,
         )
         return file_name
 
@@ -70,7 +71,7 @@ class AudioGenerate:
                 sorted_results[task.index] = results[tasks.index(task)]
             return sorted_results
 
-    def merge_audio_buffers(self, audio_buffers: list[bytes]) -> str:
+    def merge_audio_buffers(self, audio_buffers: list[bytes], voice: str) -> str:
         valid_buffers = [b for b in audio_buffers if b]
         hasher = hashlib.sha256()
         for buffer in valid_buffers:
@@ -98,5 +99,6 @@ class AudioGenerate:
         if combined:
             combined.export(output_path, format="wav")
         else:
-            logger.error(f'{RED}No valid audio data to merge{RESET}')
+            logger.error(f'{BOLD}{voice}{RESET} - {RED}No valid audio data to merge{RESET}')
+            raise Exception('Invalid audio')
         return file_name
