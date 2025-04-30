a3febcbea176e795a6c65c983c6ee5d30dd936a2
Zhao KK <kikakkz@hotmail.com>
2025-04-30 17:30:56 +0800

    Process stream response

--- api/src/chat.py
+++ api/src/chat.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 import aiohttp
+import json
 
 from config import config
 from include import *
@@ -12,8 +13,11 @@ class ModelChatChoice:
     message: ChatMessage
 
     def __init__(self, api_choice: dict):
-        message = api_choice['message']
-        self.message = ChatMessage(role=message['role'], content=message['content'])
+        message = api_choice['delta']
+        if message['content'] is None:
+            self.message = None
+            return
+        self.message = ChatMessage(role=message['role'] if 'role' in message and message['role'] is not None else 'assistant', content=message['content'])
 
 class ModelChatResponse:
     choices: list[ModelChatChoice]
@@ -34,18 +38,34 @@ async def chat(
         'messages': [
             *[{ 'role': message.role, 'content': message.content } for message in messages],
             {'role': 'user', 'content': prompt}
-        ]
+        ],
+        'stream': True,
+        'max_tokens': 1024,
     }
     headers = {
         'Authorization': f'Bearer {config.api_token}',
         'Content-Type': 'application/json'
     }
 
-    logger.info(f'{BOLD}{model}{RESET} {RED}Requesting{RESET} ...')
+    logger.info(f'{BOLD}{model}{RESET} {BOLD}Requesting{RESET} ...')
 
     timeout = aiohttp.ClientTimeout(connect=10, total=30)
+    content = ''
+
     async with aiohttp.ClientSession(timeout=timeout) as session:
         async with session.post(url, json=payload, timeout=timeout, headers=headers) as response:
-            response.raise_for_status()
-            chat_response = ModelChatResponse(await response.json())
-            return chat_response.choices[0].message.content
+            async for chunk in response.content.iter_any():
+                text = chunk.decode('utf-8').strip()
+                if text.startswith('data:'):
+                    json_str = text[len('data:'):].strip()
+                    if json_str == '[DONE]':
+                        logger.info(f'{BOLD}{model}{RESET} {BOLD}Response {content[0:32]}{RESET} ...')
+                        return content
+
+                    obj = json.loads(json_str)
+                    chat_response = ModelChatResponse(obj)
+
+                    choice = chat_response.choices[0]
+                    if choice.message is None or choice.message.content is None:
+                        continue
+                    content += chat_response.choices[0].message.content
