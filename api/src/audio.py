57358de79752355d8f1dd33bf728868ed8dc570c
Zhao KK <kikakkz@hotmail.com>
2025-05-07 18:06:19 +0800

    Log audio uid

--- api/src/audio.py
+++ api/src/audio.py
@@ -19,7 +19,7 @@ class AudioGenerate:
         try:
             logger.info(f'{BOLD}{audio_uid}{RESET} {GREEN}Generating{RESET} ...')
             start_time = time.time()
-            audio_file_cid = await self.generate_audio(text, voice)
+            audio_file_cid = await self.generate_audio(text, voice, audio_uid)
             db.update_audio(audio_uid, audio_file_cid, None)
             logger.info(f'{BOLD}{audio_uid}{RESET} {GREEN}Generate success{RESET} ... elapsed {BOLD}{time.time() - start_time}{RESET}s')
         except Exception as e:
@@ -34,7 +34,7 @@ class AudioGenerate:
 
         return audio_uid
 
-    async def generate_audio(self, text: str, voice: str) -> str:
+    async def generate_audio(self, text: str, voice: str, audio_uid: str) -> str:
         simulator = db.get_simulator_with_audio_id(voice)
         if simulator is None:
             raise Exception('Invalid voice')
@@ -48,7 +48,7 @@ class AudioGenerate:
 
         cleaned_text = purify_text(text)
         chunks = chunk_text(cleaned_text)
-        audio_buffers = await self.concurrent_audio_requests(chunks, voice, audio_b64, simulator['text'])
+        audio_buffers = await self.concurrent_audio_requests(chunks, voice, audio_b64, simulator['text'], audio_uid)
 
         file_cid = self.merge_audio_buffers(
             audio_buffers=[b for b in audio_buffers if b],
@@ -58,17 +58,18 @@ class AudioGenerate:
         return file_cid
 
     async def fetch_audio(
-            self,
-            text: str,
-            voice: str,
-            session: aiohttp.ClientSession,
-            semaphore: asyncio.Semaphore,
-            index: int,
-            voice_audio_b64: str,
-            voice_audio_text: str,
-            min_delay_ms: float = 50,
-            max_delay_ms: float = 300
-            ) -> bytes:
+        self,
+        text: str,
+        voice: str,
+        session: aiohttp.ClientSession,
+        semaphore: asyncio.Semaphore,
+        index: int,
+        voice_audio_b64: str,
+        voice_audio_text: str,
+        audio_uid: str,
+        min_delay_ms: float = 50,
+        max_delay_ms: float = 300
+    ) -> bytes:
         url = 'https://kikakkz-cosy-voice-tts.chutes.ai/v1/speak'
         headers = {
             'Authorization': f'Bearer {config.api_token}',
@@ -82,10 +83,10 @@ class AudioGenerate:
 
         _uid = uuid.uuid4()
         start_time = time.time()
-        logger.info(f'{BOLD}{url}{RESET} {BOLD}Requesting{RESET} ... {_uid}')
 
         timeout = aiohttp.ClientTimeout(connect=10, total=None)
         async with semaphore:
+            logger.info(f'{BOLD}{url} - {audio_uid}{RESET} {BOLD}Requesting{RESET} ... {_uid}')
             try:
                 delay_seconds = random.uniform(min_delay_ms, max_delay_ms) / 1000 * (index + 1)
                 await asyncio.sleep(delay_seconds)
@@ -93,18 +94,18 @@ class AudioGenerate:
                 async with session.post(url, json=payload, timeout=timeout, headers=headers) as response:
                     response.raise_for_status()
                     audio_bytes = await response.read()
-                    logger.info(f'{BOLD}{url}{RESET} {GREEN}Request success{RESET} ... {BOLD}{_uid}{RESET} elapsed {BOLD}{time.time() - start_time}{RESET}s')
+                    logger.info(f'{BOLD}{url} - {audio_uid}{RESET} {GREEN}Request success{RESET} ... {BOLD}{_uid}{RESET} elapsed {BOLD}{time.time() - start_time}{RESET}s')
                     return audio_bytes
             except Exception as e:
-                logger.error(f'{BOLD}{url}{RESET} {RED}Request exception{RESET} ... {repr(e)} - {BOLD}{_uid}{RESET} elapsed {BOLD}{time.time() - start_time}{RESET}s')
+                logger.error(f'{BOLD}{url} - {audio_uid}{RESET} {RED}Request exception{RESET} ... {repr(e)} - {BOLD}{_uid}{RESET} elapsed {BOLD}{time.time() - start_time}{RESET}s')
                 raise Exception(repr(e))
 
-    async def concurrent_audio_requests(self, chunks: list[str], voice: str, voice_audio_b64: str, voice_audio_text: str) -> list[bytes]:
+    async def concurrent_audio_requests(self, chunks: list[str], voice: str, voice_audio_b64: str, voice_audio_text: str, audio_uid: str) -> list[bytes]:
         semaphore = asyncio.Semaphore(config.concurrent_audio_requests)
         async with aiohttp.ClientSession(raise_for_status=True) as session:
             tasks = []
             for idx, text in enumerate(chunks):
-                task = asyncio.create_task(self.fetch_audio(text, voice, session, semaphore, idx, voice_audio_b64, voice_audio_text))
+                task = asyncio.create_task(self.fetch_audio(text, voice, session, semaphore, idx, voice_audio_b64, voice_audio_text, audio_uid))
                 task.index = idx
                 tasks.append(task)
 
