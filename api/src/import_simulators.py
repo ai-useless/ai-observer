a1a7a6cc3e662724e0abf0bb136c545b81c06068
Zhao KK <kikakkz@hotmail.com>
2025-05-12 11:12:50 +0000

    Support check uploaded audio

--- api/src/import_simulators.py
+++ api/src/import_simulators.py
@@ -2,7 +2,9 @@ import json
 import requests
 import hashlib
 import base64
+import asyncio
 
+from audio import generator
 from config import config
 from db import db
 from include import *
@@ -62,7 +64,7 @@ def fetch_kikakkz_avatar():
     url = 'https://avatars.githubusercontent.com/u/13128505?v=4&size=40'
     return fetch_avatar_then_save(url, 'wechat')
 
-def main():
+async def main():
     with open(config.simulators_file, 'r', encoding='utf-8') as f:
         simulators = json.load(f)
 
@@ -74,6 +76,12 @@ def main():
         avatar_cid = fetch_avatar_then_save(simulator['avatar'], 'simulator')
         (audio_cid, audio_b64, audio_url) = fetch_audio_then_save(simulator['audio'])
         audio_text = audio_2_text(audio_b64)
+
+        try:
+            await generator.audio_request_one('我是尊贵的测试文本！', audio_cid, audio_url, audio_text)
+        except Exception as e:
+            raise e
+
 
         db.new_simulator(
             config.kikakkz_wechat_openid,
@@ -91,4 +99,4 @@ def main():
             True if simulator['host'] is True else False)
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
