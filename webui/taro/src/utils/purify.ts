fa29c6df878bf65903132b091a31008577f8bd86
Zhao KK <kikakkz@hotmail.com>
2025-05-14 08:58:31 +0800

    Remove style for audio text

--- webui/taro/src/utils/purify.ts
+++ webui/taro/src/utils/purify.ts
@@ -3,6 +3,7 @@ import * as cheerio from 'cheerio'
 export const purifyText = (html: string): string => {
   const $ = cheerio.load(html)
   $('script').remove()
+  $('style').remove()
   $('sup').remove()
   $('think').remove()
   return $.text()
