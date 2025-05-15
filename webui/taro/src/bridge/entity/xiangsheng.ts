1856390e7e5940840eda22b25376dcfc4b70dcb3
Zhao KK <kikakkz@hotmail.com>
2025-05-15 14:53:47 +0800

    Merge code

--- webui/taro/src/bridge/entity/xiangsheng.ts
+++ webui/taro/src/bridge/entity/xiangsheng.ts
@@ -91,11 +91,7 @@ export class EXiangsheng {
 
   onTopicsResponse = (message: xiangshengWorker.TopicsResponsePayload) => {
     this.subTopics.push(...message.topics)
-<<<<<<< Updated upstream
-    if (this.subTopicIndex < 0) this.start()
-=======
     if (this.subTopicIndex < 0 || this.subTopics.length <= this.subTopicIndex) this.start()
->>>>>>> Stashed changes
   }
 
   generateTopics = () => {
