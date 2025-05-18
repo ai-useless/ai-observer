e86fd71c180eb5bac700e87ce97b2e89ef8093c6
Zhao KK <kikakkz@hotmail.com>
2025-05-18 18:55:00 +0800

    Add share to friend

--- webui/taro/src/app.config.ts
+++ webui/taro/src/app.config.ts
@@ -1,4 +1,6 @@
 export default {
+  enableShareAppMessage: true,
+  enableShareTimeline: true,
   pages: [
     'pages/index/IndexPage',
     'pages/seminar/SeminarPage',
--- webui/taro/src/components/post/Post.vue
+++ webui/taro/src/components/post/Post.vue
@@ -28,7 +28,7 @@
           </View>
         </View>
         <View style='margin-top: 4px; font-size: 12px; color: gray;'>{{ _prompt }}</View>
-        <Button class='plain-btn' size='mini' plain style='margin-top: 4px;' open-type='share'>
+        <Button class='plain-btn' size='mini' plain style='margin-top: 4px;' open-type='share' :data-id='index' :data-title='_prompt'>
           <Image :src='share' style='width: 16px; height: 16px;' />
         </Button>
       </View>
@@ -46,10 +46,10 @@
 </template>
 
 <script setup lang='ts'>
-import { View, Image, Text, Button } from '@tarojs/components'
+import { View, Image, Button } from '@tarojs/components'
 import { computed, nextTick, onMounted, ref, watch } from 'vue'
 import { dbBridge, entityBridge } from 'src/bridge'
-import Taro from '@tarojs/taro'
+import Taro, { ShareAppMessageObject, ShareAppMessageReturn, ShareTimelineReturnObject, useShareAppMessage, useShareTimeline } from '@tarojs/taro'
 import { model } from 'src/localstores'
 
 import ComplexInput from '../input/ComplexInput.vue'
@@ -58,12 +58,16 @@ import { send, share } from 'src/assets'
 
 const prompt = ref('忐忑又充满希望')
 
+const timelinePrompt = ref(prompt.value)
+const timelinePosterPath = ref(undefined as unknown as string)
+
 const audioInput = ref(false)
 const audioError = ref('')
 
 const inputHeight = ref(0)
 const memeHeight = ref(0)
 const scrollTop = ref(999999)
+const generating = ref(false)
 
 interface Image {
   prompt: string
@@ -77,29 +81,63 @@ watch(imageCount, async () => {
   scrollTop.value += 1
 })
 
+watch(generating, () => {
+  if (generating.value) {
+    Taro.showLoading({
+      title: '正在创作...'
+    })
+  } else {
+    Taro.hideLoading()
+  }
+})
+
 const generate = (_prompt: string) => {
   entityBridge.EImage.generate(_prompt, '唯美而意境悠远', false, '', true, true, (_image: string) => {
     const _images = images.value.get(_prompt) || []
     _images.push(_image)
     images.value.set(_prompt, _images)
-    Taro.hideLoading()
+    generating.value = false
+
+    if (_images.length >= 9) {
+      if (timelinePosterPath.value) {
+        const fs = Taro.getFileSystemManager()
+        fs.removeSavedFile({
+          filePath: timelinePosterPath.value,
+          success: () => {
+            sharePoster(_prompt).then((posterPath) => {
+              timelinePrompt.value = _prompt
+              timelinePosterPath.value = posterPath as string
+            }).catch((e) => {
+              console.log(`Failed generate poster: ${e}`)
+            })
+          },
+          fail: (e) => {
+            console.log(`Failed remove: ${e}`)
+          }
+        })
+        return
+      }
+      sharePoster(_prompt).then((posterPath) => {
+        timelinePosterPath.value = posterPath as string
+      }).catch((e) => {
+        console.log(`Failed generate poster: ${e}`)
+      })
+    }
   })
 }
 
 const refine = (_prompt: string) => {
-  Taro.showLoading({
-    title: '正在创作...'
-  })
+  generating.value = true
+
   entityBridge.EChat.refine(_prompt, dbBridge._Model.topicModelId()).then((__prompt) => {
     if (!__prompt) {
-      Taro.hideLoading()
       return
     }
     for (let i = 0; i < 9; i++) {
       generate(__prompt)
     }
   }).catch((e) => {
-    Taro.hideLoading()
+    generating.value = false
     console.log(`Failed refine: ${e}`)
   })
 }
@@ -135,6 +173,48 @@ onMounted(async () => {
     memeHeight.value = Taro.getWindowInfo().windowHeight - 32
   }
   model.Model.getModels()
+})
+
+const sharePoster = async (title: string) => {
+  const canvasId = 'canvas'
+  const canvasCtx = Taro.createCanvasContext(canvasId)
+
+  const _images = images.value.get(title)
+  if (!_images || !_images.length) return undefined
+
+  for (let i = 0; i < _images.length; i++) {
+    canvasCtx.drawImage(_images[i], i / 3, 300 * (i % 3), 300, 300)
+  }
+
+  return new Promise((resolve, reject) => {
+    canvasCtx.draw(false, () => {
+      Taro.canvasToTempFilePath({
+        canvasId,
+        success: res => resolve(res.tempFilePath),
+        fail: e => reject(e)
+      })
+    })
+  })
+}
+
+useShareAppMessage(async (res: ShareAppMessageObject) => {
+  const dataset = res.target ? (res.target as Record<string, Record<string, string>>).dataset || {} : {}
+  const dataTitle = dataset.title
+
+  const posterPath = await sharePoster(dataTitle)
+  return {
+    title: dataTitle,
+    path: '/pages/post/PostPage',
+    imageUrl: posterPath
+  } as ShareAppMessageReturn
+})
+
+useShareTimeline(() => {
+  return {
+    title: timelinePrompt.value,
+    path: '/pages/post/PostPage',
+    imageUrl: timelinePosterPath.value
+  } as ShareTimelineReturnObject
 })
 
 </script>
