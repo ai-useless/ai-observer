5d0c502963fee87c88857f1e0bd9fb3b8857ce15
Zhao KK <kikakkz@hotmail.com>
2025-05-20 14:47:24 +0800

    Better background of xiangsheng

--- webui/taro/src/components/xiangsheng/MessageCard.vue
+++ webui/taro/src/components/xiangsheng/MessageCard.vue
@@ -2,17 +2,15 @@
   <View
     :stamp='message.datetime'
     :text='[message.message]'
-    text-color='grey-9'
-    bg-color='grey-2'
     style='width: 100%;'
   >
-    <View v-if='message.first' style='font-size: 16px; padding: 16px 0; font-weight: 600; text-align: center;'>
+    <View v-if='message.first' style='font-size: 16px; padding: 16px 0; font-weight: 600; text-align: center; color: white;'>
       {{ message.topic }}
     </View>
-    <View style='padding-bottom: 4px; line-height: 24px; margin-top: 8px; padding-top: 4px; padding-bottom: 8px; display: flex; flex-wrap: wrap; justify-content: center; align-items: center;'>
+    <View style='padding-bottom: 4px; line-height: 24px; padding-top: 4px; padding-bottom: 4px; display: flex; flex-wrap: wrap; justify-content: center; align-items: center; color: white;'>
       <View>{{ message.participator.role === dbModel.Role.HOST ? '逗哏' : '捧哏' }}</View>
       <Image :src='message.simulator.simulator_avatar_url' mode='scaleToFill' style='margin-left: 4px; height: 24px; width: 24px; border-radius: 50%;' />
-      <View style='width: calc(100% - 24px - 8px - 32px); margin-left: 4px;'>
+      <View style='max-width: calc(100% - 24px - 8px - 32px); margin-left: 4px;'>
         <rich-text :nodes='message.message' user-select style='font-size: 14px;' />
       </View>
     </View>
