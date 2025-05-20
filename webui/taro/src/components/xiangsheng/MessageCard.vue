047904778809f114da55e3481a2c5bfa8ea11403
Zhao KK <kikakkz@hotmail.com>
2025-05-20 14:54:04 +0800

    Better script

--- webui/taro/src/components/xiangsheng/MessageCard.vue
+++ webui/taro/src/components/xiangsheng/MessageCard.vue
@@ -10,7 +10,7 @@
     <View style='padding-bottom: 4px; line-height: 24px; padding-top: 4px; padding-bottom: 4px; display: flex; flex-wrap: wrap; justify-content: center; align-items: center; color: white;'>
       <View>{{ message.participator.role === dbModel.Role.HOST ? '逗哏' : '捧哏' }}</View>
       <Image :src='message.simulator.simulator_avatar_url' mode='scaleToFill' style='margin-left: 4px; height: 24px; width: 24px; border-radius: 50%;' />
-      <View style='max-width: calc(100% - 24px - 8px - 32px); margin-left: 4px;'>
+      <View style='width: calc(100% - 24px - 8px - 32px); margin-left: 4px;'>
         <rich-text :nodes='message.message' user-select style='font-size: 14px;' />
       </View>
     </View>
