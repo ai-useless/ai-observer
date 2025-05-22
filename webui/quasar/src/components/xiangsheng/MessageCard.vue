9cc08f1f446d15f8ec224aa2b187e3d1dbb1c642
Zhao KK <kikakkz@hotmail.com>
2025-05-22 20:28:27 +0800

    Support xiangsheng message card

--- webui/quasar/src/components/xiangsheng/MessageCard.vue
+++ webui/quasar/src/components/xiangsheng/MessageCard.vue
@@ -10,7 +10,7 @@
     <div style='padding-bottom: 4px; line-height: 18px; padding-top: 4px; padding-bottom: 4px; display: flex; flex-wrap: wrap; justify-content: center; align-items: center;' class='text-grey-8'>
       <div>{{ message.participator.role === dbModel.Role.HOST ? '逗哏' : '捧哏' }}</div>
       <q-img :src='message.simulator.simulator_avatar_url' style='margin-left: 4px; height: 24px; width: 24px; border-radius: 50%;' />
-      <div style='width: calc(100% - 24px - 8px - 32px); margin-left: 4px;'>
+      <div style='width: calc(100% - 24px - 16px - 32px); margin-left: 16px;'>
         <div style='font-size: 16px;'>
           {{ message.message }}
         </div>
