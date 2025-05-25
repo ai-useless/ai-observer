82bef36133a0695a6a38a929a30ec46d5d9388de
Zhao KK <kikakkz@hotmail.com>
2025-05-25 15:49:13 +0800

    Add meme generator

---
+++ meme-generator/run_meme_generator.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+OUTPUT_DIR=$SCRIPT_DIR/.meme.output
+
+mkdir -p $OUTPUT_DIR
+cd $OUTPUT_DIR
+git clone https://github.com/MemeCrafters/meme-generator-rs.git
+cd meme-generator-rs
+
+docker build . -t meme-generator-rs
+
+VOLUME_NAME=meme-images
+
+volume=$(docker volume ls -q -f name=^${VOLUME_NAME}$)
+
+docker run -d --name=meme-generate-rs -p 2233:2233 --restart always meme-generator-rs
