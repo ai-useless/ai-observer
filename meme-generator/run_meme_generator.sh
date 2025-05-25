131886511b0af79a4009cf594ceeea913467386b
hong-t <th13564039482@163.com>
2025-05-25 17:54:24 +0800

    Support run meme generator

--- meme-generator/run_meme_generator.sh
+++ meme-generator/run_meme_generator.sh
@@ -13,12 +13,18 @@ OUTPUT_DIR=$SCRIPT_DIR/.meme.output
 mkdir -p $OUTPUT_DIR
 cd $OUTPUT_DIR
 git clone https://github.com/MemeCrafters/meme-generator-rs.git
+
 cd meme-generator-rs
+cp -v $SCRIPT_DIR/cargo.config .
+cp -v $SCRIPT_DIR/Dockerfile .
 
 docker build . -t meme-generator-rs
 
 VOLUME_NAME=meme-images
 
 volume=$(docker volume ls -q -f name=^${VOLUME_NAME}$)
+if [ "x$volume" == "x" ]; then
+  docker volume create $VOLUME_NAME
+fi
 
-docker run -d --name=meme-generate-rs -p 2233:2233 --restart always meme-generator-rs
+docker run -d -v $VOLUME_NAME:/data --name=meme-generate-rs -p 2233:2233 --restart always meme-generator-rs
