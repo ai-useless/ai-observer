f1f174def49f5cad01ae3d6ba86c3dcb3c6a18fa
Zhao KK <kikakkz@hotmail.com>
2025-05-25 22:31:02 +0800

    Remove unused style

--- webui/quasar/src/components/search/Search.vue
+++ webui/quasar/src/components/search/Search.vue
@@ -18,7 +18,7 @@
                 {{ modelName(result.modelId) }}
               </div>
             </div>
-            <div v-html='result.message' class='full-width' style='margin: 0 !important;' />
+            <div v-html='result.message' class='full-width' />
           </div>
         </q-scroll-area>
         <div class='flex justify-center items-center'>
