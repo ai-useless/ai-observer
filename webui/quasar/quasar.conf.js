0f4c43c37abbad09fb4a5e62b7ecea13c5a9477a
Zhao KK <kikakkz@hotmail.com>
2025-05-27 17:54:26 +0800

    Try to reduce memory

--- webui/quasar/quasar.conf.js
+++ webui/quasar/quasar.conf.js
@@ -81,6 +81,24 @@ module.exports = configure(function (ctx) {
         viteConf.resolve.alias.buffer = 'buffer/'
 
         return viteConf
+      },
+
+      sourcemap: false,
+      chainWebpack (chain) {
+        chain.optimization.splitChunks({
+          chunks: 'all',
+          maxInitialRequests: Infinity,
+          minSize: 0,
+          cacheGroups: {
+            vendor: {
+              test: /[\\/]node_modules[\\/]/,
+              name (module) {
+                const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+                return `npm.${packageName.replace('@', '')}`;
+              }
+            }
+          }
+        });
       }
     },
 
