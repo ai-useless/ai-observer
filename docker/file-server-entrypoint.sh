1a57d4583f3e1fabd307656a65bf9e8d1afa8d1c
hong-t <th13564039482@163.com>
2025-05-28 17:39:04 +0800

    Correct import

--- docker/file-server-entrypoint.sh
+++ docker/file-server-entrypoint.sh
@@ -9,4 +9,4 @@ curl -L -o /data/downloads/qiaomuyu.gif https://github.com/hong-t/avatars/raw/re
 curl -L -o /data/downloads/xiangshengwutai.png https://github.com/hong-t/avatars/raw/refs/heads/master/xiangshengwutai.png
 curl -L -o /data/downloads/yuanzhuotaolun.png https://github.com/hong-t/avatars/raw/refs/heads/master/yuanzhuotaolun.png
 
-nginx -g daemon off;
+nginx -g 'daemon off;'
