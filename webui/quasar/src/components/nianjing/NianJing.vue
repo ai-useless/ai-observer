e2f015226242723d57271d768eb1429900e797b3
Zhao KK <kikakkz@hotmail.com>
2025-05-30 11:09:39 +0800

    Only play once

--- webui/quasar/src/components/nianjing/NianJing.vue
+++ webui/quasar/src/components/nianjing/NianJing.vue
@@ -313,7 +313,7 @@ const typing = () => {
       if (typingMessage.value && typingMessage.value.first) {
         generating.value = false
         inputPrompt.value = ''
-        if (disablePlay && !musicPlayer.value && music.value?.length && lrcLetters.value) {
+        if (disablePlay && !musicPlayer.value) {
           window.clearInterval(typingTicker.value)
           typingTicker.value = -1
           AudioPlayer.play(music.value).then((player) => {
